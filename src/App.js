5325bc2a755cf01b2cf9b951b5c7ecdfeb710840
david <jesus.s.chacon2005@gmail.com>
2023-09-06 18:09:47 -0500

    integrador

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import Cards from './components/Cards.jsx';
+import Nav from './components/Nav.jsx'
+import { useState } from 'react';
+import axios from 'axios';
+
+
+
+function App() {
+
+   const [characters, setCharacters] = useState([]);
+
+   const onSearch = (id) => {
+      axios(`https://rickandmortyapi.com/api/character/${id}`)
+      .then(({ data }) => {
+         if (data.name) {
+            setCharacters((oldChars) => [...oldChars, data]);
+         } else {
+            window.alert('¡No hay personajes con este ID!');
+         }
+      });
+   }
+
+
+   
+   const onClose = (id) =>{
+      const characterFiltered = characters.filter(character => character.id !== Number(id))
+      setCharacters(characterFiltered)
+   }
+  
+
+   return (
+      <div className='App'>
+         <Nav onSearch ={onSearch} />
+         <Cards characters={characters} onClose={onClose} />
+        
+
+      </div>
+   );
+}
+
+export default App;
