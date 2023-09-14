8a3b419949ee25b26fd403f5aeba13088e7dd7dd
david <jesus.s.chacon2005@gmail.com>
2023-09-14 17:41:44 -0500

    ridux

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,30 +1,37 @@
 import React from 'react';
 import { connect, useDispatch } from 'react-redux';
 import Card from '../Card/Card';
-import { ORDER, FILTER } from '../../redux/action-types';
+import { filterCards, orderCards } from '../../redux/actions';
 
-const Favorites = (props) => {
-    const { myFavorites } = props;  
+const Favorites = ({ myFavorites }) => {
 
+    const dispatch = useDispatch ();
     const removeFromFavorites = (id) => {
         
     };
+    const handleOrder = (event) =>{
+        dispatch(orderCards(event.targer.value));
+    }
+
+    const handleFilter = (event) => {
+        dispatch(filterCards(event.target.value))
+    }
 
     return (
         <div>
 
-        <select>
+        <select onChange={handleOrder}>
             <option value="Ascendente">Ascendente</option>
             <option value="descendente">descendente</option>
         </select>
-        <select>
+
+        <select onChange={handleFilter}>
             <option value="Male">Male</option>
             <option value="Female">Female</option>
             <option value="Genderless">Genderless</option>
             <option value="unknown">unknown</option>
         </select>
         
-            Personajes Favoritos.
             {
                 myFavorites && myFavorites.map(character => (
                     <Card 
