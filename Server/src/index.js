3fafe51d21e912bb29825b0f93e21a4b1e191c20
david <jesus.s.chacon2005@gmail.com>
2023-09-19 18:44:20 -0500

    Client

--- Server/src/index.js
+++ Server/src/index.js
@@ -5,7 +5,13 @@ const http = require("http");
 http.createServer((req, res)=>{
     res.setHeader('Access-Control-Allow-Origin', '*');
     if(req.url.includes('/rickandmorty/character')){
-        
+        const id = req.url.split('/').at(-1)
+
+        const characterFound = data.find((character) =>
+        character.id === +id)
+
+        return res.writeHead(200, {"Content-type": "application/json"})
+        .end(JSON.stringify(characterFound))
     }
 })
 .listen(3001, 'localhost')
