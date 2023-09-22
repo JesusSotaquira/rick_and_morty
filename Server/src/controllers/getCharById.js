736f8e8f1c7294b2f998129a573081dd1f3e9bf8
david <jesus.s.chacon2005@gmail.com>
2023-09-21 22:56:12 -0500

    promesas

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -1,6 +1,28 @@
 const axios = require('axios');
 
-function getCharById (res, id){
+function getCharById(res, id) {
     axios.get(`https://rickandmortyapi.com/api/character/${id}`)
-}
-module.exports = getCharById;
+        .then(response => response.data)
+        .then(({name, gender, species, origin, image, status }) => {
+           const character = {
+                id,
+                name,
+                gender,
+                species,
+                origin: origin.name,
+                image,
+                status
+           }
+
+           return res 
+                 .writeHead(200, {"Content-type": "application/json"})
+                 .end(JSON.stringify(character))
+        })
+        .catch(error => {
+            return res
+            .writeHead(500, {"Content-type": "text/plain"})
+            .end(JSON.stringify(character))
+        })
+    }
+
+module.exports = {getCharById};
